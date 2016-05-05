51e5ff2bc6bdea4e2290f72023feafc0d4781d00
nao20010128nao <nao20010128@gmail.com>
2016-05-05 14:44:53 +0900

    実装

--- src/com/nao20010128nao/f2ip/DeleteBreaklinesReader.java
+++ src/com/nao20010128nao/f2ip/DeleteBreaklinesReader.java
@@ -1,64 +1,56 @@
 package com.nao20010128nao.f2ip;
 
-import java.io.BufferedReader;
+import java.io.FilterInputStream;
 import java.io.FilterReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
 public class DeleteBreaklinesReader extends FilterReader {
-
-	public DeleteBreaklinesReader(Reader reader) {
-		// TODO 自動生成されたコンストラクター・スタブ
-		super(new BufferedReader(reader));
-	}
-
 	public DeleteBreaklinesReader(InputStream reader) {
 		// TODO 自動生成されたコンストラクター・スタブ
-		this(new InputStreamReader(reader));
-	}
-
-	@Override
-	public int read() throws IOException {
-		// TODO 自動生成されたメソッド・スタブ
-		int r = super.read();
-		if (r == '\r' | r == '\n')
-			return read();
-		return r;
-	}
-
-	@Override
-	public int read(char[] b, int off, int len) throws IOException {
-		// TODO 自動生成されたメソッド・スタブ
-		if (b == null)
-			throw new NullPointerException();
-		else if (off < 0 || len < 0 || len > b.length - off)
-			throw new IndexOutOfBoundsException();
-		else if (len == 0)
-			return 0;
-
-		int c = read();
-		if (c == -1)
-			return -1;
-		b[off] = (char) c;
-
-		int i = 1;
-		try {
-			for (; i < len; i++) {
-				c = read();
-				if (c == -1)
-					break;
-				b[off + i] = (char) c;
+		super(new InputStreamReader(new FilterInputStream(reader) {
+			@Override
+			public int read() throws IOException {
+				int r = super.read();
+				if (r == -1)
+					return -1;
+				if (r == '\r' | r == '\n')
+					return read();
+				return r;
 			}
-		} catch (IOException ee) {
-		}
-		return i;
-	}
 
-	@Override
-	public int read(char[] b) throws IOException {
-		// TODO 自動生成されたメソッド・スタブ
-		return read(b, 0, b.length);
+			@Override
+			public int read(byte b[]) throws IOException {
+				return read(b, 0, b.length);
+			}
+
+			@Override
+			public int read(byte b[], int off, int len) throws IOException {
+				if (b == null)
+					throw new NullPointerException();
+				else if (off < 0 || len < 0 || len > b.length - off)
+					throw new IndexOutOfBoundsException();
+				else if (len == 0)
+					return 0;
+
+				int c = read();
+				if (c == -1)
+					return -1;
+				b[off] = (byte) c;
+
+				int i = 1;
+				try {
+					for (; i < len; i++) {
+						c = read();
+						if (c == -1)
+							break;
+						b[off + i] = (byte) c;
+					}
+				} catch (IOException ee) {
+				}
+				return i;
+			}
+		}));
 	}
 }
--- src/com/nao20010128nao/f2ip/Finder.java
+++ src/com/nao20010128nao/f2ip/Finder.java
@@ -1,7 +1,54 @@
 package com.nao20010128nao.f2ip;
 
-public class Finder {
-	public static void main(String... args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Vector;
+import java.util.zip.GZIPInputStream;
 
+public class Finder {
+	public static void main(String... args) throws IOException {
+		final BigInteger STATUS_UNIT = new BigInteger("100000");
+		System.out.println("Loading...");
+		Vector<InputStream> lis = new Vector<>();
+		ClassLoader currentClassLoader = Finder.class.getClassLoader();
+		for (int i = 1; i <= 45; i++)
+			lis.add(currentClassLoader.getResourceAsStream("com.nao20010128nao.f2ip.pi.".replace('.', '\\')
+					+ "pi1g.txt.gz." + ("000" + i).substring(("000" + i).length() - 3, ("000" + i).length())));
+		SequenceInputStream sis = new SequenceInputStream(lis.elements());
+		GZIPInputStream gis = new GZIPInputStream(sis);
+		DeleteBreaklinesReader dbr = new DeleteBreaklinesReader(gis);
+		BufferedReader br = new BufferedReader(dbr);
+		BigInteger reached = new BigInteger("8");
+		char[] result = "20010128".toCharArray();
+		char[] data = new char[8];
+		br.read(data);
+		System.out.println("Head 8 chars:" + new String(data));
+		while (true) {
+			if (Arrays.equals(data, result)) {
+				System.out.println("\nFound at:" + reached);
+				return;
+			}
+			{
+				data[1] = data[0];
+				data[2] = data[1];
+				data[3] = data[2];
+				data[4] = data[3];
+				data[5] = data[4];
+				data[6] = data[5];
+				data[7] = data[6];
+			}
+			int r = br.read();
+			if (r == -1)
+				break;
+			data[0] = (char) r;
+			reached = reached.add(BigInteger.ONE);
+			if (reached.mod(STATUS_UNIT).equals(BigInteger.ZERO))
+				System.out.println("Reached:" + reached);
+		}
+		System.err.println("Not Found...");
 	}
 }
